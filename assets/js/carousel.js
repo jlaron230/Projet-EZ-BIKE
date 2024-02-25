d1ef5084c632968e8724391aa6ed174c1892beaf
Thesirix <renaud.d.mercier@gmail.com>
2024-02-25 18:19:48 +0100

    carousel full mecha

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -33,34 +33,86 @@ let left=document.querySelector('.left')
 let right=document.querySelector('.right')
 
 
+console.log(currentIndex);
 
 
+if (currentIndex === 0) {
+    left.classList.add('gray');
+}
 
 
 
 document.querySelector('.left').addEventListener("click", function () {
-    currentIndex = (currentIndex - 1 + array.length) % array.length;
-    document.querySelector(".grimg").src = array[currentIndex];
-    dots.forEach(dot => dot.style.backgroundColor = "");
-    dots[currentIndex].style.backgroundColor = "green";
-    left.classList.add('active')
-    console.log(left);
-    setTimeout(function() {
-        left.classList.remove('active');
-      }, 150);
+    if (currentIndex > 0) { 
+        currentIndex = (currentIndex - 1 + array.length) % array.length;
+        document.querySelector(".grimg").src = array[currentIndex];
+        dots.forEach(dot => dot.style.backgroundColor = "");
+        dots[currentIndex].style.backgroundColor = "green";
+        left.classList.add('active');
+        setTimeout(function() {
+            left.classList.remove('active');
+        }, 150);
+    }
+    console.log(currentIndex); 
+
+    if (currentIndex == 0) {
+        left.classList.add('inactive');
+        left.classList.add('gray');
+    } else {
+        left.classList.remove('inactive');
+        left.classList.remove('gray')
+    }
+
+    if (currentIndex == array.length - 1) {
+        right.classList.add('inactive');
+        right.classList.add('gray');
+    } else {
+        right.classList.remove('inactive');
+        right.classList.remove('gray');
+    }
+
 });
 
+
+
+
+
+
+
+
 document.querySelector('.right').addEventListener("click", function () {
-    currentIndex = (currentIndex + 1) % array.length;
-    document.querySelector(".grimg").src = array[currentIndex];
-    dots.forEach(dot => dot.style.backgroundColor = "");
-    dots[currentIndex].style.backgroundColor = "green";
-    right.classList.add('active')
-    setTimeout(function() {
-        right.classList.remove('active');
-      }, 150);
+    if (currentIndex < array.length - 1) { 
+        currentIndex = (currentIndex + 1) % array.length;
+        document.querySelector(".grimg").src = array[currentIndex];
+        dots.forEach(dot => dot.style.backgroundColor = "");
+        dots[currentIndex].style.backgroundColor = "green";
+        right.classList.add('active');
+        setTimeout(function() {
+            right.classList.remove('active');
+        }, 150);
+    }
     
-});
+    console.log(currentIndex);
+
+    if (currentIndex == 0) {
+        left.classList.add('inactive');
+        left.classList.add('gray');
+    } else {
+        left.classList.remove('inactive');
+        left.classList.remove('gray')
+    }
+
+
+    if (currentIndex == array.length - 1) {
+        right.classList.add('inactive');
+        right.classList.add('gray');
+    } else {
+        right.classList.remove('inactive');
+        right.classList.remove('gray');
+    }
+
+});    
+
 
 
 
