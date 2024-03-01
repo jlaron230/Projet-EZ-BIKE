4e83dc3d412685b3de4498d226a9b542f46c2439
thesirix <renaud.d.mercier@gmail.com>
2024-03-01 01:29:11 +0100

    carte: icon marker plus algo couleur icon

--- assets/js/trouver-un-velib.js
+++ assets/js/trouver-un-velib.js
@@ -1,13 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+// la SEULE doc https://leafletjs.com/
+
 
         const apikey="?&key=MjE0ZDNmMGEtNGFkZS00M2FlLWFmMWItZGNhOTZhMWQyYzM2"
         const http="https://api.omega.fifteen.eu/gbfs/2.2/marseille/en/"
-
-
-
     
-        //_____________________________________________________________________________________________________
+//_________________________________________________________________________________________________________________________________________
+
+
+        // déclaration des icones
+        var LeafIcon = L.Icon.extend({
+            options: {
+
+            shadowUrl:"image/leaf-shadow.png",
+            iconSize:     [30, 50], // size of the icon
+            shadowSize:   [50, 64], // size of the shadow
+            iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+            shadowAnchor: [4, 62],  // the same for the shadow
+            popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+        }
+    });
+
+    var greenIcon = new LeafIcon({iconUrl: 'image/leaf-green.png'}),
+    orangeIcon = new LeafIcon({iconUrl: 'image/leaf-orange.png'}),
+    redIcon = new LeafIcon({iconUrl: 'image/leaf-red.png'})
+   
 
         var map = L.map('map').setView([43.300000, 5.400000], 13);   //Marseille
 
@@ -17,38 +35,32 @@ document.addEventListener('DOMContentLoaded', function () {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributeurs'
         }).addTo(map);
 
-        //________________________________________________________________________________________________________
+//________________________________________________________________________________________________________________________________________
 
   
 
- function affichepoint(lat, lon) {
-
-            return L.marker([lat, lon]).addTo(map);
-            
-
-        }
-
-function popup(marker,infodata,i,availabledata) {
-    
-    marker.bindPopup(`<b>${infodata.data.stations[i].name}</b><br>${availabledata.data.stations[i].
-        num_bikes_available} vélo disponible`).openPopup();
+ function affichepoint(lat, lon,i,availabledata) {
+    let num=availabledata.data.stations[i].num_bikes_available
+if (num===0){
+    return L.marker([lat, lon],{icon: redIcon}).addTo(map);
 }
+else if (num<3){
+    return L.marker([lat, lon],{icon: orangeIcon}).addTo(map);
+ }
+ else{
+    return L.marker([lat, lon],{icon: greenIcon}).addTo(map);
+ }
+          }
 
+function popup(marker,infodata,i,availabledata) {marker.bindPopup(`<b>${infodata.data.stations[i].name}</b><br>${availabledata.data.stations[i].num_bikes_available} vélo disponible`).openPopup();}
 
     
-
-        
-    
-    
-        async function fetchvelo() {
+    async function fetchvelo() {
             const availableresponse = await fetch(`${http}station_status.json${apikey}`)
             const availabledata = await availableresponse.json()
     
             const inforesponse = await fetch(`${http}station_information.json${apikey}`)
             const infodata = await inforesponse.json()
-    
-
-            console.log(infodata.data.stations[0].name);
     
             //______________________________________
             console.log(availabledata);
@@ -59,21 +71,16 @@ function popup(marker,infodata,i,availabledata) {
             //________________________________________
     
 
-
             infodata.data.stations.forEach((element,i) => {  //param foreach (element, index , tableau parent) => {}
                 let latitude = element.lat;
                 let longitude = element.lon;
                 
     
-                let marker=affichepoint(latitude, longitude);  
+                let marker=affichepoint(latitude, longitude,i,availabledata);  
                 popup(marker,infodata,i,availabledata)
             });
         }
       
-
-    
-
-
     
         fetchvelo()
     })
